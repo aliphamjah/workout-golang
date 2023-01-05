2d7df3422b672986b0f8eda023598b6540f6dc8e
Alip Hamjah <aliphamjah@gmail.com>
2023-01-05 09:31:15 +0800

    add: operator logic #1

--- basic/main.go
+++ basic/main.go
@@ -87,9 +87,24 @@ func constdata()  {
 
 func operator() {
 	fmt.Println("-> operator")
+
+	// ratio
 	var value = (((2 + 6) % 3) * 4 - 2) / 3
 	var isEqual = (value == 2)
-
 	fmt.Printf("nilai %d (%t) \n", value, isEqual)
+
+	// logic
+	var left = false
+	var right = true
+
+	var leftAndRight = left && right
+	fmt.Printf("left && right \t(%t) \n", leftAndRight)
+
+	var leftOrRight = left || right
+	fmt.Printf("left || right \t(%t) \n", leftOrRight)
+
+	var leftReverse = !left
+	fmt.Printf("!left \t\t(%t) \n", leftReverse)
+
 	fmt.Println()
 }
