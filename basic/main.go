2ed2eee45331df9cbb6414a003d1f6f7fa595a4f
Alip Hamjah <aliphamjah@gmail.com>
2023-01-07 12:14:43 +0800

    add: looping #1

--- basic/main.go
+++ basic/main.go
@@ -10,6 +10,7 @@ func main() {
 	constdata()
 	operator()
 	conditional()
+	looping()
 }
 
 func helloWord()  {
@@ -196,5 +197,71 @@ func conditional()  {
 		}
 	}
 
+	fmt.Println()
+}
+
+func looping()  {
+	fmt.Println("-> looping")
+
+	// basic
+	fmt.Println("> basic")
+	for i := 0; i < 5; i++ {
+		fmt.Println("Angka", i)
+	}
+
+	// for keyword
+	fmt.Println("> for keyword")
+	var i = 0
+	for i < 5 {
+		fmt.Println("Angka", i)
+		i++
+	}
+
+	// for keyword with break
+	fmt.Println("> for keyword with break")
+	i = 0
+	for {
+		fmt.Println("Angka", i)
+
+		i++
+		if i == 5 {
+			break
+		}
+	}
+
+	// break & continue keyword
+	fmt.Println("> break & continue keyword")
+	for i := 1; i <= 10; i++ {
+		if i % 2 == 1 {
+			continue
+		}
+
+		if i > 8 {
+			break
+		}
+
+		fmt.Println("Angka", i)
+	}
+
+	// loop on loop
+	fmt.Println("> loop on loop")
+	for i := 0; i < 5; i++ {
+		for j := i; j < 5; j++ {
+			fmt.Print(j, " ")
+		}
+		fmt.Println()
+	}
+
+	// outerloop
+	fmt.Println("> outerloop")
+	outerLoopLabel:
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 5; j++ {
+			if i == 2 {
+				break outerLoopLabel
+			}
+			fmt.Print("matriks [", i, "][", j, "]", "\n")
+		}
+	}
 	fmt.Println()
 }
