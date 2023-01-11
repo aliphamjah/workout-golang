2a1eb2118beb6a614201f41ab961877bef1a6f1b
Alip Hamjah <aliphamjah@gmail.com>
2023-01-11 22:29:28 +0800

    add: function as params #1

--- basic-2/main.go
+++ basic-2/main.go
@@ -91,6 +91,18 @@ func function() {
     fmt.Println("found \t:", howMany)    // 9
     fmt.Println("value \t:", theNumbers) // [2 3 0 3 2 0 2 0 3]
 
+	fmt.Println("> function as params")
+	var data = []string{"wick", "jason", "ethan"}
+    var dataContainsO = filter(data, func(each string) bool {
+        return strings.Contains(each, "o")
+    })
+    var dataLenght5 = filter(data, func(each string) bool {
+        return len(each) == 5
+    })
+    fmt.Println("data asli \t\t:", data)
+    fmt.Println("filter ada huruf \"o\"\t:", dataContainsO)
+    fmt.Println("filter jumlah huruf \"5\"\t:", dataLenght5)
+
 	fmt.Println()
 }
 
@@ -141,4 +153,16 @@ func findMax(numbers []int, max int) (int, func() []int) {
     return len(res), func() []int {
         return res
     }
+}
+
+// alias cloure
+type FilterCallback func(string) bool
+func filter(data []string, callback FilterCallback) []string {
+    var result []string
+    for _, each := range data {
+        if filtered := callback(each); filtered {
+            result = append(result, each)
+        }
+    }
+    return result
 }
