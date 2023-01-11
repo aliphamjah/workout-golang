8a0236239e82029611e07a3f77425e9643d19c8a
Alip Hamjah <aliphamjah@gmail.com>
2023-01-11 10:46:21 +0800

    add: multiplle return function #1

--- basic-2/main.go
+++ basic-2/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"strings"
 	"math/rand"
+	"math"
 )
 
 func main() {
@@ -20,13 +21,23 @@ func function() {
 
 	fmt.Println("> return")
 	var randomValue int
+    randomValue = randomWithRange(2, 10)
+    fmt.Println("random number:", randomValue)
+    randomValue = randomWithRange(2, 10)
+    fmt.Println("random number:", randomValue)
+    randomValue = randomWithRange(2, 10)
+    fmt.Println("random number:", randomValue)
 
-    randomValue = randomWithRange(2, 10)
-    fmt.Println("random number:", randomValue)
-    randomValue = randomWithRange(2, 10)
-    fmt.Println("random number:", randomValue)
-    randomValue = randomWithRange(2, 10)
-    fmt.Println("random number:", randomValue)
+	fmt.Println("> return 2 value")
+	var diameter float64 = 15
+    var area, circumference = calculate(diameter)
+	fmt.Printf("wide lingkaran\t\t: %.2f \n", area)
+    fmt.Printf("circumference lingkaran\t: %.2f \n", circumference)
+
+	fmt.Println("> predefinedreturn value")
+    area, circumference = calculate_1(diameter)
+	fmt.Printf("wide lingkaran\t\t: %.2f \n", area)
+    fmt.Printf("circumference lingkaran\t: %.2f \n", circumference)
 
 	fmt.Println()
 }
@@ -39,4 +50,21 @@ func printMessage(message string, arr []string) {
 func randomWithRange(min, max int) int {
 	var value = rand.Int() % (max - min + 1) + min
 	return value
+}
+
+func calculate(d float64) (float64, float64) {
+    // count wide
+    var area = math.Pi * math.Pow(d / 2, 2)
+    // count circumference
+    var circumference = math.Pi * d
+
+    // return back 2 value
+    return area, circumference
+}
+
+func calculate_1(d float64) (area float64, circumference float64) {
+    area = math.Pi * math.Pow(d / 2, 2)
+    circumference = math.Pi * d
+
+    return
 }
