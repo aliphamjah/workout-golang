f2276f7999bd8d330af736ecca90d4d9bc7dd4c6
Alip Hamjah <aliphamjah@gmail.com>
2023-01-11 11:56:19 +0800

    add: closure function #1

--- basic-2/main.go
+++ basic-2/main.go
@@ -44,6 +44,52 @@ func function() {
     var msg = fmt.Sprintf("Average : %.2f", avg)
     fmt.Println(msg)
 
+	fmt.Println("> closure")
+	var getMinMax = func(n []int) (int, int) {
+        var min, max int
+        for i, e := range n {
+            switch {
+            case i == 0:
+                max, min = e, e
+            case e > max:
+                max = e
+            case e < min:
+                min = e
+            }
+        }
+        return min, max
+    }
+    var numbers = []int{2, 3, 4, 3, 4, 2, 3}
+    var min, max = getMinMax(numbers)
+    fmt.Printf("data : %v\nmin  : %v\nmax  : %v\n", numbers, min, max)
+
+	fmt.Println("> immediately-invoked function expression (IIFE)")
+	var numbers_1 = []int{2, 3, 0, 4, 3, 2, 0, 4, 2, 0, 3}
+    var newNumbers = func(min int) []int {
+        var r []int
+        for _, e := range numbers_1 {
+            if e < min {
+                continue
+            }
+            r = append(r, e)
+        }
+        return r
+    }(3)
+
+    fmt.Println("original number :", numbers_1)
+    fmt.Println("filtered number :", newNumbers)
+
+    fmt.Println("> closure as return")
+	var max_1 = 3
+    var numbers_2 = []int{2, 3, 0, 4, 3, 2, 0, 4, 2, 0, 3}
+    var howMany, getNumbers = findMax(numbers_2, max_1)
+    var theNumbers = getNumbers()
+
+    fmt.Println("numbers\t:", numbers_2)
+    fmt.Printf("find \t: %d\n\n", max)
+
+    fmt.Println("found \t:", howMany)    // 9
+    fmt.Println("value \t:", theNumbers) // [2 3 0 3 2 0 2 0 3]
 
 	fmt.Println()
 }
@@ -83,4 +129,16 @@ func calculate_2(numbers... int) float64 {
 
     var avg = float64(total) / float64(len(numbers))
     return avg
+}
+
+func findMax(numbers []int, max int) (int, func() []int) {
+    var res []int
+    for _, e := range numbers {
+        if e <= max {
+            res = append(res, e)
+        }
+    }
+    return len(res), func() []int {
+        return res
+    }
 }
