0fa1b3d2f706bf67027f80e38213035f1f33bfdc
Alip Hamjah <aliphamjah@gmail.com>
2023-01-12 18:57:18 +0800

    refactor: folder layout

---
+++ basic/1-hello-world.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main()  {
+	fmt.Println("-> Hello Word")
+
+	var firstName string = "alip"
+	var lastName string
+	lastName = "hamjah"
+	greeting := "Selamat Pagi"
+
+	fmt.Println("Hello ", firstName, lastName, ",", greeting)
+
+	fmt.Println()
+}
